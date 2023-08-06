35928d7f5b09a293cc87d5a1234eac31cfea66fe
RaghuSharma14 <140615301+RaghuSharma14@users.noreply.github.com>
2023-08-06 21:27:15 +0530

    Update namedfunction.js

--- Day3/namedfunction.js
+++ Day3/namedfunction.js
@@ -34,3 +34,25 @@ named Function
 window.show();
 named Function
 
+
+
+//  WAP to print area and perimeter of a rectangle by taking inputs from the user using prompt by Normal function:
+
+function Area_and_Perimeter() {
+    let a=parseInt(prompt("Enter the length"));
+    // length=window.prompt();
+    let b=parseInt(prompt("Enter the width"));
+    // width=window.prompt();
+    let area=a*b;
+    let perimeter=2*(a+b);
+    console.log("Area is:",area)
+        console.log("Perimeter is: ",perimeter);
+}
+Area_and_Perimeter();
+
+//OUTPUT-
+//Enter the length : 3
+//Enter the breadth : 4
+Area is: 12
+Perimeter is:  14
+
